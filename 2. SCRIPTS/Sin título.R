d78a941e5aa6eef02dfde28a104a621092e5fa05
dannabn12 <dannab.n12@gmail.com>
2023-02-04 19:54:53 -0500

    Update Sin título.R

--- 2. SCRIPTS/Sin título.R
+++ 2. SCRIPTS/Sin título.R
@@ -2,4 +2,14 @@
 library(pacman)
 p_load(rvest,tidyverse)
 #Cargamos y creamos nuestra data
- <- read_html('https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html')
+chunk_1 <- read_html('https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html')
+view(chunk_1)
+chunk_2 <- read_html('https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_2.html')
+chunk_3 <- read_html('https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_3.html')
+chunk_4 <- read_html('https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_4.html')
+chunk_5 <- read_html('https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_5.html')
+
+merge(x = df_1, y = df_2, all = TRUE)
+
+
+df_combined <- rbind(df1, df2)
