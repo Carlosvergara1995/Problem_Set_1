60d6d47200d165208a076e3bdbcb71602e314fbf
dannabn12 <dannab.n12@gmail.com>
2023-02-11 20:34:21 -0500

    Update Modelo1.R

--- 2. SCRIPTS/Modelo1.R
+++ 2. SCRIPTS/Modelo1.R
@@ -17,42 +17,86 @@ for (i in 1:10) {
 #Eliminamos la primera columna, la cual es de indices y no se requiere
 data<-(data)[-1]
 
-#Se transforma a tipo Tibble para un mejor análisisdsi
+#Se transforma a tipo Tibble para un mejor análisis 
 data <- as_tibble(data)
 view(data)
-#*********************** exluimos los datos que nos interesan (mayores de 18 -  empleados)
-##De este primer analisis se concluye que existe cero faltantes en edad, por lo cual el primero paso es seleccionar
-## que la muestra sea mayor o igual a 18 años, para personas que se encuentran empleadas.
+
+#*********************** excluimos los datos que nos interesan (mayores de 18 -  empleados)
 data<-(data %>%
        filter(age >= 18, dsi == 0))
+
+#Analisamos la estructura de la base 
+glimpse(data)
+
+#******************** Analisis na *********************
+
+#Analisis de na por variable
+colSums(is.na(data))
+
+#Calculamos el porcentaje de los datos diferentes de NA
+sum(data$y_ingLab_m_ha > 0 & !is.na(data$y_ingLab_m_ha) )/length(data$y_ingLab_m_ha)
+# se infiere que el 43% de la base presenta datos 
+
+#eliminamos todas las filas con un valor faltante en la columna de nuestra valiable dependiente (y_ingLab_m_ha)
+df <- data[!is.na(data$y_ingLab_m_ha), ] %>% as.data.frame()
 
 #****************** separate the target variable ***
 #select variables used in the analysis
 #f(x)
-wage.features.of.interest = c(ApprovedSchoolGrade='p6210s1','age')
+df = data_frame( wage = df$y_ingLab_m_ha,ApprovedSchoolGrade= df$p6210s1, age =df$age, sizefirm=df$sizeFirm, oficio= df$oficio, totalHoursWorked=df$totalHoursWorked )
+
+# Ahora vamos a revisar la distribución de nuestra variable a predecir
+HistogramWage <- ggplot(df, aes(x = y_ingLab_m_ha)) +
+  geom_histogram( fill = "#BFEFFF") +
+  labs(x = "Salario por horas", y = "Pesos colombianos") +
+  theme_bw()
+
+##Realizamos un analisis exploratorio de valores atipicos para la varaible de interes que es y_ingLab_m_ha
+
+BoxplotWage <- ggplot(df, aes(x = "Salarios", y = y_ingLab_m_ha)) +
+  geom_boxplot(fill = "#CFDCEF", color = "#F5D8C0") +
+  ggtitle("Diagrama de Cajas de salario por hora") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+GrafDispercion <- plot(df$y_ingLab_m_ha,pch=19,col="#FFF6F5")
+
+#normalisamos los datos aplicando log
+lgwage <-log(df$y_salary_m_hu)
+df<-cbind(df,lgwage)
+
+
+BoxplotWage <- ggplot(df, aes(x = "Salarios", y = y_ingLab_m_ha)) +
+  geom_boxplot(fill = "#CFDCEF", color = "#F5D8C0") +
+  ggtitle("Diagrama de Cajas de salario por hora") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+GrafDispercion <- plot(lgwage,pch=19,col="#FFF6F5")
+
+
+# descriptive statistics
+
+
+#divide the base into train and test
+set.seed(123)
+
+id_train <- sample(1:nrow(wage.features.of.interest), size = 0.7*nrow(df2), 
+                   replace = FALSE)
+
+train <- wage.features.of.interest[id_train, ]
+test  <- wage.features.of.interest[-id_train, ]
+
+
+
 x= data %>% select (wage.features.of.interest)
 
 #y
 y <- data %>% 
-  select(y_salary_m_hu)  %>%
-  rename(HourlyWage= y_salary_m_hu)
+  select(y_ingLab_m_ha)  %>%
+  rename(HourlyWage= y_ingLab_m_ha)
 
 dat = cbind(x, y)
 
-#******************** Analisis na *********************
 
-
-#Analisis de na por variable
-colSums(is.na(dat))
-porcentajeMiss <- function(dat) {sum(is.na(dat)) / length(dat)*100}
-apply(dat, 2, porcentajeMiss)
-
-# encontramos 12748 missing values en la variables dependiente hourlywage
-# lo cual respresenta 56.30742 % de sus datos
-
-#eliminamos todas las filas con unvalor faltante en esta columna 
-dat<- dat[complete.cases(dat[,3]),]
-colSums(is.na(dat))
 
 #******************* estadisticos variables *************
 #resumen de variables
